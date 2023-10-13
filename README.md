bd0a958a5d00ac8938e096f1f5efaeebf6e0b383
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-10-13 16:22:17 +0500

    updated

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 
 ## TCP/UDP(Layer 4) Load Balancer
+
+
+```bash
+sudo groupadd loadbalancer
+sudo useradd -r -s /bin/false -g loadbalancer loadbalancer
+sudo chmod 750 /etc/load-balancer-rs/load-balancer-rs
+sudo chmod 640 /etc/load-balancer-rs/load-balancer-rs/*
+```
+
+
+
+```bash
+sudo nano /etc/systemd/system/load-balancer-rs.service
+```
+
+
+```bash
+[Unit]
+Description=Load Balancer RS
+After=network.target
+
+[Service]
+ExecStart=/etc/load-balancer-rs/load-balancer-rs
+WorkingDirectory=/opt/load-balancer-rs
+Restart=always
+User=loadbalancer
+Group=loadbalancer
+Environment="PATH=/usr/bin:/bin" "LOAD_BALANCER_CONFIG=/etc/load-balancer-rs/loadbalancer.toml"
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
+```
+
+```bash
+sudo systemctl enable load-balancer-rs
+sudo systemctl start load-balancer-rs
+sudo systemctl daemon-reload
+sudo systemctl restart load-balancer-rs
+sudo systemctl status load-balancer-rs
+```
--- src/udp_balancer.rs
+++ src/udp_balancer.rs
@@ -1,36 +1,25 @@
 use super::config::Backend;
 use std::net::{UdpSocket, SocketAddr};
-use std::thread;
-use std::str::FromStr;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-pub fn start_udp_load_balancer(address: &str, backends: Vec<Backend>) {
-    let socket = UdpSocket::bind(address).unwrap();
-    let backends_addresses: Vec<SocketAddr> = backends.iter().map(|backend| {
-        SocketAddr::from_str(&format!("{}:{}", backend.ip, backend.port)).unwrap()
-    }).collect();
+pub fn start_udp_load_balancer(address: &str, backends: Vec<Backend>) -> Result<(), std::io::Error> {
+    let socket = UdpSocket::bind(address)?;
+    println!("UDP Load Balancer started on {}", address);
 
-    let mut backend_index = 0;
+    let backend_index = Arc::new(AtomicUsize::new(0));
 
+    let mut buf = [0; 4096];
     loop {
-        let mut buf = [0; 4096];
-        let (amt, src) = socket.recv_from(&mut buf).unwrap();
+        let (amt, src) = socket.recv_from(&mut buf)?;
 
-        // Basic round-robin mechanism to choose the backend
-        let backend_address = &backends_addresses[backend_index];
-        backend_index = (backend_index + 1) % backends_addresses.len();
+        // Basic round-robin mechanism to select backend
+        let index = backend_index.fetch_add(1, Ordering::Relaxed) % backends.len();
+        let backend = &backends[index];
 
-        // Send data to the backend
-        socket.send_to(&buf[0..amt], backend_address).unwrap();
+        let backend_address = format!("{}:{}", backend.ip, backend.port).parse::<SocketAddr>().unwrap();
 
-        // Wait for the response and forward it back to the client
-        let mut buf = [0; 4096];
-        match socket.recv_from(&mut buf) {
-            Ok((amt, _)) => {
-                socket.send_to(&buf[0..amt], src).unwrap();
-            }
-            Err(e) => {
-                eprintln!("Error receiving from backend: {}", e);
-            }
-        }
+        // Forward the packet to the selected backend
+        socket.send_to(&buf[0..amt], &backend_address)?;
     }
 }
